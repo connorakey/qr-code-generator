9d9054ab5c0560567a2372b7c67311c622bdcd9b
Connor Akey <ytconnorakey@gmail.com>
2024-11-18 15:49:59 +1100

    Added optional electron support to QR Code Generator

--- start.js
+++ start.js
@@ -1,3 +1,9 @@
 // This file is incharge of starting both the frontend and the backend hosting.
 const frontend = require("./frontend.js");
 const backend = require("./backend.js");
+
+const enableElectron = true;
+
+if (enableElectron === true) {
+    require('./electron/electron.js')
+}
